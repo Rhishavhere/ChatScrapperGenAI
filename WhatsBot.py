0487d59849d88866e197e1b8211311c65c0b0c36
Rhishav <rhishh.me@gmail.com>
2024-07-19 22:25:12 +0530

    midway whats

---
+++ WhatsBot.py
@@ -0,0 +1,90 @@
+from selenium import webdriver
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+from selenium.webdriver.edge.options import Options
+from selenium.webdriver.edge.service import Service
+
+from config import EDGE_DATA_PATH
+from dotenv import load_dotenv
+
+import time
+
+import os
+
+import google.generativeai as genai
+
+load_dotenv()
+
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+generation_config = {
+  "temperature": 1,
+  "top_p": 0.95,
+  "top_k": 64,
+  "max_output_tokens": 8192,
+  "response_mime_type": "text/plain",
+}
+
+model = genai.GenerativeModel(
+  model_name="gemini-1.5-flash",
+  generation_config=generation_config,
+  system_instruction="Your are a chatbot but dont identify yourself . you will play my role and reply to my messages sent by my friends on behave of me . I am rhishav . chat in a casual style like whatsapp chat style ",
+)
+
+history=[] 
+
+# opening app
+
+options =Options()
+options.add_argument(EDGE_DATA_PATH)
+options.add_experimental_option("detach",True)
+
+service=Service(executable_path="msedgedriver.exe")
+driver=webdriver.Edge(service=service,options=options)
+
+driver.get("https://web.whatsapp.com/")
+driver.maximize_window()
+time.sleep(10)
+
+def openChat(contact_name):
+    search_box = driver.find_element(By.XPATH, '//*[@id="side"]/div[1]/div/div[2]/div[2]/div/div[1]/p')
+    search_box.clear()
+    search_box.send_keys(contact_name)
+    search_box.send_keys(Keys.RETURN)
+    time.sleep(5)
+
+def getLastMessage():
+    messages = driver.find_elements(By.XPATH, '//*[@id="main"]/div[3]/div/div[2]/div[3]/div[41]/div/div/div[1]/div[1]/div[1]/div/div[1]/div/span[1]/span')
+    text=messages[-1].get_attribute("innerText").strip()
+    print(text)
+    return text
+
+    
+
+def replyBack():
+
+  text=getLastMessage()
+
+  chat_session = model.start_chat(
+    history=history
+  )
+
+  response = chat_session.send_message(text)
+
+  reply=response.text
+  # print(reply)
+
+  history.append({"role": "user","parts":[text]})
+  history.append({"role": "model","parts":[reply]})
+
+  input=driver.find_element(By.XPATH,'//*[@id="column-center"]/div/div/div[4]/div/div[1]/div/div[8]/div[1]/div[1]')
+  input.send_keys(reply)
+  input.send_keys(Keys.ENTER)
+
+name='Aditi'
+openChat(name)
+while True:
+ replyBack()
+ time.sleep(5)
